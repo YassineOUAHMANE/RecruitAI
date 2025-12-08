bfa94dc7b27f146bc4f8eb3ff79b5745e15f0cdf
Houssam Moussaoui <moussaoh@grenoble-inp.org>
2025-12-08 23:53:03 +0100

    .

--- src/frontend/src/utils/helpers.js
+++
@@ -1,9 +0,0 @@
-export const formatDate = (dateString) => {
-  const date = new Date(dateString);
-  return date.toLocaleString('en-US', { 
-    month: 'short', 
-    day: 'numeric', 
-    hour: '2-digit', 
-    minute: '2-digit' 
-  });
-};
